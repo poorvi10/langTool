c3a270aa30e55287dca02fcc1fb739d95c1faede
Poorvi <prvdubey@gmail.com>
2017-12-07 23:33:55 +0530

    final modification

--- spellChecker/server.js
+++ spellChecker/server.js
@@ -7,36 +7,35 @@ var fs = require('fs');
 app.use(bodyParser.json()); 
 
 app.use(express.static(__dirname + '/public'));
+var filename = '';
 
 var Storage = multer.diskStorage({
     destination: function (req, file, callback) {
-        callback(null, "./Images");
+        callback(null, "./files");
     },
     filename: function (req, file, callback) {
+        filename = file.fieldname + "_" + Date.now() + "_" + file.originalname;
         callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
     }
 });
 
-var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count 
+var upload = multer({ storage: Storage }).array("fileUploader", 1); //Field name and max count 
 app.get("/", function (req, res) { 
     res.sendFile(__dirname + "public/index.html"); 
 }); 
 
-
-  
 app.post("/api/Upload", function (req, res) {
-    upload(req, res, function (err) { 
+    upload(req, res, function (err) {
         if (err) { 
             return res.end("Something went wrong!"); 
-        } 
+        }
         //return res.end("File uploaded sucessfully!."); 
-        fs.readFile('./Images/imgUploader_1512549090455_test.txt', function(err, data) {
+        fs.readFile('./files/'+filename, function(err, data) {
             res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
             return res.end(data);
         });
     }); 
-}); 
+});
 
 app.listen(2000, function (a) { 
     console.log("Listening to port 2000"); 
